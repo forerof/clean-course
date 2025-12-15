e287fb45ac7c0a39684bf975c077ad3b3f661cf1
= <=>
2025-12-15 16:38:13 -0500

    Add initial implementation of movie and actor management functions

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,46 @@
+(() => {
+
+    // función para obtener información de una película por Id
+    function getInformationOfMovieById( id: string ) {
+        console.log({ id });
+    }
+
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getAllMovieActors( movieId: string ) {
+        console.log({ movieId });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getBioActorById( id: string ) {
+        console.log({ id });
+    }
+
+    interface Movie {
+        cast: string[];
+        description: string;
+        rating: number;
+        title: string;
+    }
+    
+    // Crear una película
+    function createMovie({ title, description, rating, cast }: Movie ) {
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createNewActor( fullName: string, birthdate: Date ): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( fullName === 'fernando' ) return false;
+
+        console.log('Crear actor');
+        return true;        
+
+    }
+
+    
+
+
+})();
